1f7a396c421af47c469c9bd3748685a189b82661
kwanmyeong lee <79038842+kwanmyeong-lee@users.noreply.github.com>
2021-07-07 16:41:42 +0900

    fist

---
+++ JLYL_ws/lylj/src/test/java/com/it/lylj/LyljApplicationTests.java
@@ -0,0 +1,13 @@
+package com.it.lylj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LyljApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
