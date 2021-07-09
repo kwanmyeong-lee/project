c3ef3d97b5811575faadd3384eeded39d20af7eb
kwanmyeong lee <79038842+kwanmyeong-lee@users.noreply.github.com>
2021-07-09 18:52:40 +0900

    navUpdate

--- JLYL_ws/lylj/src/main/java/com/it/lylj/email/controller/EmailController.java
+++ JLYL_ws/lylj/src/main/java/com/it/lylj/email/controller/EmailController.java
@@ -3,6 +3,7 @@ package com.it.lylj.email.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -13,8 +14,10 @@ public class EmailController {
 
 	
 	@RequestMapping("/emailMain")
-	public void email() {
+	public void email(Model model) {
 		logger.info("이메일 페이지");
+		model.addAttribute("navNo", 1);
+		
 	}
 	
 	@RequestMapping("/emailWrite")
