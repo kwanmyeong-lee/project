ab371a5ba193c00d9554ef2dbc9040973ca1ea3b
이준경 <away0419@naver.com>
2021-07-11 22:38:12 +0900

    Revert "Revert "예약스케줄""
    This reverts commit 53d38f0f52cfea3be2c939f86bfe2faacb3dec8e.

--- JLYL_ws/lylj/src/main/webapp/resources/js/schedule/write.js
+++ JLYL_ws/lylj/src/main/webapp/resources/js/schedule/write.js
@@ -4,4 +4,23 @@ $(function(){
 		
 	});
 	
+	$('#cal-add').click(function(){
+		$('#myModaladd').modal('show');
+	});
+	
+	$('#cal-edit').click(function(){
+		$('#myModaledit').modal('show');
+	});
+	
+	$('#cal-delete').click(function(){
+		$('#myModaldelete').modal('show');
+	});
+	
+	$('.list-span').click(function(){
+		if($(this).prev('.ckSch').prop('checked')){
+			$(this).prev('.ckSch').prop('checked',false)
+		}else{
+			$(this).prev('.ckSch').prop('checked',true)
+		}
+	});
 });
