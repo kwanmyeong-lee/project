bdb3f3d2eb18b361e0e2b8f083177bf9bee87fd8
Yooh <77115479+yooh0201@users.noreply.github.com>
2021-07-13 16:56:31 +0900

    수정(주소록, 게시판, top.jsp)

--- JLYL_ws/lylj/src/main/java/com/it/lylj/addressbook/controller/addressBookController.java
+++ JLYL_ws/lylj/src/main/java/com/it/lylj/addressbook/controller/addressBookController.java
@@ -3,6 +3,7 @@ package com.it.lylj.addressbook.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,22 +12,24 @@ public class addressBookController {
 	private static final Logger logger=LoggerFactory.getLogger(addressBookController.class);
 	
 	@RequestMapping("/addressBookMain")
-	public String bookMain() {
+	public String bookMain(Model model) {
 		logger.info("주소록 메인 페이지");
-		
+		model.addAttribute("navNo", 5);
 		return "addressBook/addressBookMain";
 	}
 
 	@RequestMapping("/addressBookWrite")
-	public String bookWrite() {
+	public String bookWrite(Model model) {
 		logger.info("주소록 추가 페이지");
+		model.addAttribute("navNo", 5);
 		
 		return "addressBook/addressBookWrite";
 	}
 
 	@RequestMapping("/addressBookEdit")
-	public String bookEdit() {
+	public String bookEdit(Model model) {
 		logger.info("주소록 수정 페이지");
+		model.addAttribute("navNo", 5);
 		
 		return "addressBook/addressBookEdit";
 	}
