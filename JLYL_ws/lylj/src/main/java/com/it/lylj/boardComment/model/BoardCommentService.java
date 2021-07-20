dc0fd9160236d7f87be6c1e94d3871253d53774b
Yooh <77115479+yooh0201@users.noreply.github.com>
2021-07-21 00:30:58 +0900

    게시판 상세보기+댓글 수정

--- JLYL_ws/lylj/src/main/java/com/it/lylj/boardComment/model/BoardCommentService.java
+++ JLYL_ws/lylj/src/main/java/com/it/lylj/boardComment/model/BoardCommentService.java
@@ -1,5 +1,8 @@
 package com.it.lylj.boardComment.model;
 
+import java.util.List;
+
 public interface BoardCommentService {
-	int insertComment(int boardNo);
+	int insertComment(BoardCommentVO commVo);
+	List<BoardCommentVO> selectByNo(int boardNo);
 }
