e7affdc72a76d20e78ed53abdcaeae60a768d5ef
coding-kevin <gisungj5027@gmail.com>
2021-07-24 01:34:02 +0900

    이메일 미리보기 추가

--- JLYL_ws/lylj/src/main/java/com/it/lylj/email/controller/EmailController.java
+++ JLYL_ws/lylj/src/main/java/com/it/lylj/email/controller/EmailController.java
@@ -4,15 +4,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.it.lylj.email.model.EmailService;
 import com.it.lylj.email.model.EmailVO;
-import com.it.lylj.emp.model.EmpVO;
 
 import lombok.RequiredArgsConstructor;
 
@@ -42,11 +40,11 @@ public class EmailController {
 		logger.info("emailDetail");
 		model.addAttribute("navNo", 2);
 	}
-	
 	@RequestMapping("/emailPreview")
-	public String emailPreview(@ModelAttribute EmailVO vo, Model model) {
-		logger.info("미리보기, vo={},",vo);
+	public String emailPreview(@ModelAttribute EmailVO vo
+			, Model model) {
+		logger.info("vo={}",vo);
 		model.addAttribute("vo", vo);
-		return "/email/emailPreview";
+		return "email/emailPreview";
 	}
 }
