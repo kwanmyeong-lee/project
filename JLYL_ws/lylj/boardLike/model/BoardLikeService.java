fc5961830489b930463ff11e0a81b546e81faf58
Yooh <77115479+yooh0201@users.noreply.github.com>
2021-07-27 23:22:50 +0900

    boardLike 중복 제거

--- JLYL_ws/lylj/boardLike/model/BoardLikeService.java
+++
@@ -1,8 +0,0 @@
-package com.it.lylj.boardLike.model;
-
-public interface BoardLikeService {
-	int insertLike(BoardLikeVO likeVo);
-	int deleteLike(BoardLikeVO likeVo );
-	int selectLikeCnt(int boardNo);
-	BoardLikeVO selectData(int boardNo);
-}
