923e3c9c6fb986c4252141fa18ef37e02c8f2f8f
Yooh <77115479+yooh0201@users.noreply.github.com>
2021-07-29 17:44:06 +0900

    주소록 유효성 + 게시판 오류 수정

--- JLYL_ws/lylj/src/main/java/com/it/lylj/addBook/model/AddBookVO.java
+++ JLYL_ws/lylj/src/main/java/com/it/lylj/addBook/model/AddBookVO.java
@@ -12,10 +12,14 @@ public class AddBookVO {
 	private int empNo;
 	private String addressBookName;
 	private String addressBookTel;
+	private String addressBookTel2;
+	private String addressBookTel3;
 	private String addressBookMaile;
 	private String addressBookPosition;
 	private String addressBookOfficeName;
 	private String addressBookOfficeTel;
+	private String addressBookOfficeTel2;
+	private String addressBookOfficeTel3;
 	private String addressBookGender;
 	private int addressFolderNo;
 }
