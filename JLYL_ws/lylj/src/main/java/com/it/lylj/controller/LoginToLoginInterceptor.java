93b9c63623a12c207830c254c0eb94920c80f3af
이준경 <away0419@naver.com>
2021-07-29 19:47:22 +0900

    error

---
+++ JLYL_ws/lylj/src/main/java/com/it/lylj/controller/LoginToLoginInterceptor.java
@@ -0,0 +1,47 @@
+package com.it.lylj.controller;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class LoginToLoginInterceptor extends HandlerInterceptorAdapter{
+	
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		String empNos =(String)request.getSession().getAttribute("empNo");
+		
+		if(!(empNos==null || empNos.isEmpty())) {
+			request.setAttribute("msg", "로그아웃을 하세요");
+			request.setAttribute("url", "/index");
+			RequestDispatcher dispatcher
+			= request.getRequestDispatcher("/WEB-INF/views/common/message.jsp");
+			dispatcher.forward(request, response);
+			
+			return false;
+	
+		}else {
+			
+			return true;
+		}
+	}
+
+
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		super.postHandle(request, response, handler, modelAndView);
+	}
+
+
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		super.afterCompletion(request, response, handler, ex);
+	}
+}
